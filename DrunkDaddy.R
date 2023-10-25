1571afa2356109b66d60968766f0f0d600262d80
hnaganob <hnaganob@gmail.com>
2023-10-25 12:15:50 -0400

    cleaning

--- DrunkDaddy.R
+++ DrunkDaddy.R
@@ -16,23 +16,21 @@ DrunkDaddy <- function(AdjMat, Step = 10000, Home = 1, Neg = FALSE) {
 
   # Number of visit at each node
   VisitSum <- tabulate(Path, nbins = dim(Adj)[1])
-  # Breaks <- 1:(ncol(Adj)+1) - 0.5
-  # H <- hist( Path , breaks=Breaks , plot=F )
+  # Breaks <- 1:(ncol(Adj) + 1) - 0.5
+  # H <- hist(Path, breaks = Breaks, plot = F)
   # VisitSum <- H$counts
 
   # Number of visit at each node
-
   VisitSumNeg <- as.vector(-diff(table(
     rep_len(c(1, -1), Step),
     factor(Path, seq(dim(Adj)[1]))
   )))
-  # as.numeric( 1:Step %% 2 == 0 )
-  # VisitSumNeg <- rep(NA,ncol(Adj))
-  # for(i in 1:ncol(Adj) )
+  # as.numeric(1:Step %% 2 == 0)
+  # VisitSumNeg <- rep(NA, ncol(Adj))
+  # for (i in 1:ncol(Adj))
   # {
-  #   VisitSumNeg[i] <- sum( ifelse( c( 1:Step %% 2 == 0 ) , 1 , -1 )[ Path==i ] )
+  #   VisitSumNeg[i] <- sum(ifelse(c(1:Step %% 2 == 0), 1, -1)[Path == i])
   # }
-
 
   return(list(
     Path = Path,
